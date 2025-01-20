f626fcf93674843f856bd811d37bfb064037860a
saki30 <s6u6s024@gmail.com>
2025-01-20 23:58:51 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       $score = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+
+       echo $score / 10;
+       ?>
+   </p>
+</body>
+
+</html>
