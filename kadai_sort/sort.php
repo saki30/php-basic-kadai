57b3ba9a5796796493b33750c1ed2c2b821f5e58
saki30 <s6u6s024@gmail.com>
2025-01-29 19:38:56 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $nums = [15, 4, 18, 23, 10 ];
+        sort_2way($nums, true);
+        sort_2way($nums, false);
+
+        function sort_2way($array, $order) {
+            if($order == true){
+                echo '昇順にソートします。' . '<br>';
+                sort($array);
+            }else{
+                echo '降順にソートします。' . '<br>';
+                rsort($array);
+            }
+
+            foreach ($array as $data){
+                echo $data . '<br>';
+            }
+        }
+        ?>
+    </p>
+</body>
+
+</html>
